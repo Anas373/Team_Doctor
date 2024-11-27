b7b77679c6a172eac58fef2d8b1208110108be85
anas ich <ichanas867@gmail.com>
2024-11-27 23:57:10 +0100

    dernier

---
+++ back-end/monprojet/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>le comptee a était créé</title>
+</head>
+<body>
+    <h1>Bonjour {{ $username }},</h1>
+    <p>votre compte q était créé avec succés!</p>
+    <p>Bienvenue sur notre plateforme.  Vous pouvez désormais explorer le site et réserver des consultations avec des médecins.</p>
+</body>
+</html>
--- src/Component/Contactus.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contactus = () => {
-  return (
-    <div className='text-center text-5xl  text-blue-500 font-bold'>Contactus</div>
-  )
-}
-
-export default Contactus
